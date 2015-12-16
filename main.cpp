f4c510431960a687e7dd682e6d3373c5cd54ea5b
Brian R. Bondy <netzen@gmail.com>
2015-12-16 08:43:42 -0500

    Fix remaining lint problems

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "BloomFilter.h"
 #include <iostream>
+#include "BloomFilter.h"
+
 using std::cout;
 using std::endl;
 
